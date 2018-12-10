4df50f71ebf9855f0860ff1b8970298fdf2bf290
Simon Date <simondate@yahoo.co.uk>
2018-12-10 15:12:38 +0100

    Replaced menu background with menu video

---
+++ js/adapt-menu-video.js
@@ -0,0 +1,35 @@
+define(["coreJS/adapt"], function(Adapt) {
+
+    var Adapt = require('coreJS/adapt');
+    var Backbone = require('backbone');
+
+    var menuVideo = Backbone.View.extend({
+
+        className: "extension",
+
+        initialize: function() {}
+    });
+
+    Adapt.on("pageView:ready", function() {
+        if(!Adapt.course.get('_menuVideo')) return;
+        if (Adapt.course.get('_menuVideo')._menuHeader._applyToPage || !(Adapt.course.get('_menuVideo')._menuHeader._link === "")) {
+          $('.page-header').css('background', 'url(' + Adapt.course.get('_menuVideo')._menuHeader._link + ')');
+          $('.page-header').css('background-size', 'cover');
+        }
+    });
+    Adapt.on("menuView:ready", function() {
+      if(!Adapt.course.get('_menuVideo')) return;
+        if (!(Adapt.course.get('_menuVideo')._menuHeader._link === "")) {
+            $('.menu-header').css('background', 'url(' + Adapt.course.get('_menuVideo')._menuHeader._link + ')');
+            $('.menu-header').css('background-size', 'cover');
+        }
+        if (!(Adapt.course.get('_menuVideo')._backgroundLink === "")) {
+            $('.menu-container-inner').css('background', 'url(' + Adapt.course.get('_menuVideo')._backgroundLink + ')');
+            $('.menu-container-inner').css('padding-bottom', '22%');
+            $('.menu-container-inner').css('background-size', 'cover');
+            $('.menu-container-inner').css('background-position-y', '300px');
+        }
+    });
+
+    return menuVideo;
+});
